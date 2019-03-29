89b3057324bb36f5892603d769523f3ef1e4d9b0
Alan Amanov <nickrodness@gmail.com>
2019-03-29 20:30:08 +0600

    init commit

---
+++ src/core/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/alan/main/work/development/backend/diploma/src/core/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
