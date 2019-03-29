53ca1a1d15ba7258b2e91786d1761ff83c9f1aee
Alan Amanov <nickrodness@gmail.com>
2019-03-29 20:46:30 +0600

    removed venv

--- src/core/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/media/alan/main/work/development/backend/diploma/src/core/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
