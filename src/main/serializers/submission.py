b862faa3fe352d256af14b433f4b9821bc8e55fa
Alan Amanov <nickrodness@gmail.com>
2019-05-20 08:17:41 +0600

    fix course participant

--- src/main/serializers/submission.py
+++ src/main/serializers/submission.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
 
 from main.models import Submission, Task, CourseParticipant
+from .course_participant import CourseParticipantSerializer
 from .result import ResultSerializer
 
 
 class SubmissionSerializer(serializers.ModelSerializer):
     task = serializers.PrimaryKeyRelatedField(many=False, queryset=Task.objects.all())
-    course_participant = serializers.PrimaryKeyRelatedField(many=False, queryset=CourseParticipant.objects.all())
+    course_participant = CourseParticipantSerializer()
     results = ResultSerializer(required=False, many=True)
 
     class Meta:
