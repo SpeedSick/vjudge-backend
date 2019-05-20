d346bce2f7344339eda0e6a317847edad2500708
Alan Amanov <nickrodness@gmail.com>
2019-05-20 08:29:51 +0600

    added my percentage to the task

--- src/main/serializers/task.py
+++ src/main/serializers/task.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from main.models import Task
+from main.models import Task, Submission
 from .submission import SubmissionSerializer
 from .result import ResultSerializer
 
@@ -8,7 +8,19 @@ from .result import ResultSerializer
 class TaskSerializer(serializers.ModelSerializer):
     created = serializers.DateField(required=False)
     modified = serializers.DateField(required=False)
+    my_percentage = serializers.SerializerMethodField(required=False)
     submissions = SubmissionSerializer(many=True, required=False)
+
+    def get_my_percentage(self, instance):
+        if self.context['request'].user.is_anonymous:
+            return None
+        submission = instance.submissions.filter(course_participant__student=self.context['request'].user.id).last()
+        if submission is None:
+            return None
+        result = submission.results.last()
+        if result is None:
+            return None
+        return result.score
 
     class Meta:
         model = Task
